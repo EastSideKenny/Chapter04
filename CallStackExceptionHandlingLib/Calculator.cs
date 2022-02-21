03eb9ed514e4658a97287ce2aa79a5d3bfeae77c
Jerome Jacob <Jerome56jj@hotmail.com>
2022-02-21 22:04:01 +0100

    Testing / Debugging

---
+++ CallStackExceptionHandlingLib/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+namespace Packt;
+
+public class Calculator
+{
+    public static void Gamma() // public so it can be called from outside
+    {
+        WriteLine("In Gamma");
+        Delta();
+    }
+
+    private static void Delta() // private so it c an only be called internally
+    {
+        WriteLine("In Delta");
+        File.OpenText("Bad file path");
+    }
+
+}
