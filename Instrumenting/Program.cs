d42756ca6d9259aeba00fc087fe2909efecf7853
Jerome Jacob <Jerome56jj@hotmail.com>
2022-02-21 16:08:09 +0100

    Trace / appsettings.json

---
+++ Instrumenting/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+
+//write to a text file in the project folder
+Trace.Listeners.Add(new TextWriterTraceListener(
+    File.CreateText(Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.DesktopDirectory), "log.txt"))));
+
+// text writer is buffered, so this option calls
+// Flush() on a ll listeners after writing
+Trace.AutoFlush = true;
+
+Debug.WriteLine("Debug says, I am watching!");
+Trace.WriteLine("Trace says, I am watching!");
+
+ConfigurationBuilder builder = new();
+
+builder.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json",
+    optional: true, reloadOnChange: true);
+
+IConfigurationRoot configuration = builder.Build();
+
+TraceSwitch ts = new(
+    displayName: "PacktSwitch",
+    description: "This switch is set via a JSON config.");
+
+configuration.GetSection("PacktSwitch").Bind(ts);
+
+Trace.WriteLine(ts.TraceError, "Trace error");
+Trace.WriteLine(ts.TraceWarning, "Trace warning");
+Trace.WriteLine(ts.TraceInfo, "Trace information");
+Trace.WriteLine(ts.TraceVerbose, "Trace verbose");
+
